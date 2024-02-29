d8e6356cac90d9aafa8fc3039a01919f4d464738
Syabil76 <142871006+Syabil76@users.noreply.github.com>
2024-02-29 20:57:17 +0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,12 +1,29 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 import socket
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#AF_INET specifies IPV4, SOCK_STREAM specifies connection (TCP/IP)
 
-#where AF_INET = the ipv4 address of the server and the SOCK_STREAM is the socket type
+host = socket.gethostbyname()
+#specifies host name and address etc.
+#OR 
+#host = 192.168.0.147 or otherwise
 
-server.bind((socket.gethostname(), 1234))
-server.listen(5)
+port = 444
 
+serversocket.bind(host, port)
+#binds the host TO the port 
 
+serversocket.listen(3)
+#specifies how many requests allowed
+
+while True:
+    clientsocket, address = serversocket.accept()
+    #accepts connection
+    print(f"recieved connection from {str(address)}")
+
+    message = 'Welcome to server, sockets are fun' + "\r\n"
+    clientsocket.send(message.encode('ascii'))
+
+    clientsocket.close()
